d15cc7c24f6a31301e782193a6864023e52be920
Hinnerk Kuennen <hinnerk.kuennen@gmail.com>
2022-11-22 11:04:15 +0100

    work in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Clearing database..."
+User.destroy_all
+Survivalist.destroy_all
+Metting.destroy_all
+
+puts "Creating users..."
+20.times do
+  User.create({email: Faker::Internet.email, name: "", password: Faker::Internet.password, city: Faker::Address.city})
+end
+puts "Users created!"
+
+users = User.all
+
+puts "Creating survivalists..."
+12.times do
+  Survivalist.create({name: "", category: "", skills: "", street: Faker::Address.street_address, postcode: Faker::Address.zip_code, city: Faker::Address.city})
+end
+puts "Survivalists created!"
+
+survivalists = Survivalist.all
+
+# puts "Creating meetings..."
+# Meeting.create({})
+# puts "Meetings created!"
+
+puts "Database seeded!"
