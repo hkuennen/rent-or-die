7c5b548090f75da8e05578f2eb9fa9162240d6c1
Hinnerk Kuennen <hinnerk.kuennen@gmail.com>
2022-11-24 22:56:51 +0100

    delete for authenticated user added

--- app/policies/meeting_policy.rb
+++ app/policies/meeting_policy.rb
@@ -3,9 +3,25 @@ class MeetingPolicy < ApplicationPolicy
     def resolve
       scope.where(user: user)
     end
+  end
 
-    def show?
-      true
-    end
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    record.user == user
+  end
+
+  def update?
+    record.user == user
+  end
+  
+  def destroy?
+    record.user == user
   end
 end
